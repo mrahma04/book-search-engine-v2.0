595ce4eee24f0dfaa2ac83301f21539892361494
Mohammad Rahman <mbrahman@gmail.com>
2022-05-21 12:30:31 -0400

    added functionality to create users with passwords

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,22 @@
-const User = require('../models/User')
+const { User } = require('../models')
 
 const resolvers = {
     Query: {
         helloWorld: () => {
             return 'Hello world!'
         },
-        users: async () => {
-            return User.find()
+        users: async (parent, { username }) => {
+            // params will be sent to the Mongoose methods
+            // if there's a username...params === username
+            // otherwise...params = {}
+            const params = username ? { username } : {}
+            return User.find(params)
+        }
+    },
+    Mutation: {
+        addUser: async(parent, args) => {
+            const user = await User.create(args)
+            return user
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,17 +4,23 @@ const {gql} = require('apollo-server-express')
 // create typeDefs
 // create Query to get all users
 // create Query to get a single user
+// create User
 const typeDefs = gql`
 
     type User {
         _id: ID
         username: String
         email: String
+        password: String
     }
 
     type Query {
         helloWorld: String
-        users: [User]
+        users(username: String): [User]
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): User
     }
 `
 
